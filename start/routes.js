7210b75c782e47f92ba32c88149237399b122249
ylanbenoliel <ylanbenoliel@gmail.com>
2019-11-05 16:10:56 -0300

    Create routes to property

--- start/routes.js
+++ start/routes.js
@@ -4,4 +4,7 @@
 const Route = use('Route')
 
 Route.post('/users', 'UserController.create')
-Route.post('/sessions','SessionController.create')
+Route.post('/sessions', 'SessionController.create')
+Route.resource('properties', 'PropertyController')
+  .apiOnly()
+  .middleware('auth')
